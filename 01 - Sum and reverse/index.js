bea579d2769aba9294251ffb8a85be61d6fabe41
Peter Murphy <peter.francis.murphy@gmail.com>
2018-09-16 14:27:58 -0400

    Renamed sumDownFrom to fib :)

--- 01 - Sum and reverse/index.js
+++ 01 - Sum and reverse/index.js
@@ -1,5 +1,3 @@
 const sum = undefined
 
 const reverse = undefined
-
-module.exports = { sum, reverse }
--- 02 - Proper tail call/index.js
+++ 02 - Proper tail call/index.js
@@ -1,14 +1,13 @@
 "use strict"
 
-const sumDownFrom = num => {
+const fib = num => {
   if (num === 0) return 0
-  else return num + sumDownFrom(num - 1)
+  else return num + fib(num - 1)
 }
 
-// This is in "tail position" -- i.e., the function call is the only thing in the return statement
-const sumDownFromPTC = (num, acc = 0) => {
+// This is in "proper tail call" form, meaning
+// the function call is the only thing after `return`
+const fibPTC = (num, acc = 0) => {
   if (num === 0) return acc
-  else return sumDownFromPTC(num - 1, acc + num)
+  else return fibPTC(num - 1, acc + num)
 }
-
-module.exports = { sumDownFrom, sumDownFromPTC }

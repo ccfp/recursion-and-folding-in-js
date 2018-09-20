a7dc1f75f25cfa79e06b2a7e6a2694fd57b2aa0e
Peter Murphy <peter.francis.murphy@gmail.com>
2018-09-20 12:39:50 -0400

    JK, should have been tri not fib

--- 02 - Proper tail call/index.js
+++ 02 - Proper tail call/index.js
@@ -1,13 +1,13 @@
 "use strict"
 
-const fib = num => {
+const tri = num => {
   if (num === 0) return 0
-  else return num + fib(num - 1)
+  else return num + tri(num - 1)
 }
 
 // This is in "proper tail call" form, meaning
 // the function call is the only thing after `return`
-const fibPTC = (num, acc = 0) => {
+const triPTC = (num, acc = 0) => {
   if (num === 0) return acc
-  else return fibPTC(num - 1, acc + num)
+  else return triPTC(num - 1, acc + num)
 }

6d772186469450eca127343a13a333ca5a3a2f36
Peter Murphy <peter.francis.murphy@gmail.com>
2018-09-22 15:04:50 -0400

    Refactored any, sum, reverse, in terms of reduce, and made reduce pointfree

--- 2 - Simple recursive functions/any-sum-reverse.js
+++ 2 - Simple recursive functions/any-sum-reverse.js
@@ -1,28 +1,39 @@
+const isOdd = x => x % 2 === 1
+
 // const sum = ([num, ...nums], acc = 0) => {
 //   if (num === undefined) return acc
 //   return sum(nums, acc + num)
 // }
+// sum([1, 4, 9, 8])
 // -> 22
 
-const reverse = ([item, ...items], acc = []) => {
-  if (item === undefined) return acc
-  return reverse(items, [item, ...acc])
-}
-reverse(["a", "b", "c", "d"]) //?
+// const reverse = ([item, ...items], acc = []) => {
+//   if (item === undefined) return acc
+//   return reverse(items, [item, ...acc])
+// }
+// reverse(["a", "b", "c", "d"])
 // -> ["d", "c", "b", "a"]
 
-const reduce = (reducerFn, acc, [x, ...xs]) => {
+// const any = (predFn, [item, ...items], acc = false) => {
+//   if (item === undefined) return acc
+//   return any(predFn, items, predFn(item) || acc)
+// }
+// any(isOdd, [1, 4, 9, 8]) //?
+// -> true
+
+const reduce = (reducerFn, acc) => ([x, ...xs]) => {
   if (x === undefined) return acc
-  return reduce(reducerFn, reducerFn(acc, x), xs)
+  return reduce(reducerFn, reducerFn(acc, x))(xs)
 }
 
-sum = arr => reduce((acc, x) => acc + x, 0, arr)
+const sum = reduce((acc, x) => acc + x, 0)
 
 sum([1, 4, 9, 8]) //?
 
-// const any = ([bool, ...bools], acc = false) => {
-//   if (bool === undefined) return acc
-//   return any(bools, bool || acc)
-// }
+const reverse = reduce((acc, x) => [x, ...acc], [])
 
-// any([true, false, false]) //?
+reverse(["a", "b", "c", "d"]) //?
+
+const any = predFn => reduce((acc, x) => predFn(x) || acc, false)
+
+any(isOdd)([0, 1, 2, 3]) //?

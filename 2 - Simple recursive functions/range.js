7ca02a31cae08e047ebd250a78c56307f1c1e387
Peter Murphy <peter.francis.murphy@gmail.com>
2018-09-22 13:59:00 -0400

    Added unfold example

--- 2 - Simple recursive functions/range.js
+++ 2 - Simple recursive functions/range.js
@@ -1,3 +1,8 @@
-const range = (a, b) => undefined
+// This is an example of an `unfold`
+// (just to show you can do more with recursion than `fold`)
+const range = (a, b, acc = []) => {
+  if (a >= b + 1) return acc
+  return range(a + 1, b, [...acc, a])
+}
 // range(0, 5)
 // -> [0, 1, 2, 3, 4, 5]
